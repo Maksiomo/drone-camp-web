ad58df037ea2fd4dbdb98aac58866d2d1ca2afec
Vasileva Vlada <vlada.3234.vasileva@gmail.com>
2021-12-02 21:48:42 +0400

    changed menu

--- components/nav.js
+++ components/nav.js
@@ -4,9 +4,6 @@ import styles from './nav.module.css'
 export default function Nav(props) {
   return (
     <nav className={styles.nav}>
-      <Link href="/">
-        <a className={props.picked === "about" ? styles.picked : ""}>О нас</a>
-      </Link>
       <Link href="/education">
         <a className={props.picked === "education" ? styles.picked : ""}>База знаний</a>
       </Link>
@@ -15,6 +12,9 @@ export default function Nav(props) {
       </Link>
       <Link href="/test">
         <a className={props.picked === "test" ? styles.picked : ""}>Тест</a>
+      </Link>
+      <Link href="/">
+        <a className={props.picked === "about" ? styles.picked : ""}>О нас</a>
       </Link>
     </nav>
   )
