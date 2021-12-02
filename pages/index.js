1b009c23c7d939712b88dc728f2870f905aee564
Vasileva Vlada <vlada.3234.vasileva@gmail.com>
2021-12-02 19:29:04 +0400

    sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Nav from '../components/nav'
+import Sidebar from '../components/sidebar'
 
 export default function Home() {
   return (
     <div>
       <Nav/>
+      <Sidebar/>
       <main>
         Тест
       </main>
