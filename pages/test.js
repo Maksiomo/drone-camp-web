57c3123586d3ac5840d770ee50c58f3974e14bb1
Vasileva Vlada <vlada.3234.vasileva@gmail.com>
2021-12-03 13:02:54 +0400

    .

--- pages/test.js
+++ pages/test.js
@@ -6,12 +6,12 @@ import styles from '../components/test.module.css'
 export default function Test() {
   const [results, setResults] = useState("");
   let answears = [
-    ["вариант 1", "вариант 2"],
-    ["вариант 1", "вариант 2", "вариант 3"]
+    ["Положительный угол атаки или профиль выпуклый снизу", "Положительный угол атаки или профиль выпуклый сверху", "Отрицательный угол атаки или профиль выпуклый снизу", "Отрицательный угол атаки или профиль выпуклый сверху"],
+    ["Элероны", "Рули направления", "Вертикальные стабилизаторы", "Рули высоты"]
   ];
   const [questions, setQuestions] = useState ([
-        {id: 1, title: "Вопрос", answears: answears[0]},
-        {id: 2, title: "Вопрос 2", answears: answears[1]},
+        {id: 1, title: "Что необходимо для возникнования подъемной силы?", answears: answears[0]},
+        {id: 2, title: "Какие части самолета не относятся к органам управления?", answears: answears[1]},
   ])
 
   const submit = event => {
