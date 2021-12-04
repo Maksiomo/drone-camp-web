b60a8e38938b1ff81378d24437e9a8eec47e8c40
Zagarazhi <fukuroOwl@yandex.ru>
2021-12-04 09:05:20 +0400

    fast

--- pages/practices.js
+++ pages/practices.js
@@ -5,7 +5,7 @@ export default function Practices() {
     <div>
       <Nav pressed={"practices"}/>
       <main className="practices">
-        <iframe className="simulator" src="https://playcanv.as/b/azhwFd8w"></iframe>
+        <iframe className="simulator" src="https://playcanv.as/b/IF6XA5S8/"></iframe>
       </main>
     </div>
   )
